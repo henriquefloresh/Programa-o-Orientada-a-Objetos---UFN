b634222e3c7fb423e0a5f544934b89b26e880fe7
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-03-03 09:06:17 -0300

    Add Ex4.java with a loop and print statement

---
+++ Ex4.java
@@ -0,0 +1,14 @@
+package pkg;
+
+
+public class Ex4 {
+	public static void main(String[] args) {
+	
+	for(int i=0; i<100; i++) {
+		int soma=0;
+		soma=i+i;
+		System.out.println("I vale: "+soma);
+		}
+
+	}
+}
